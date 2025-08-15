2c621a7892e006039ccec7beedbb8c448c290e32
Esther Babaoye <estherbabaoye@gmail.com>
2025-08-15 01:13:57 +0100

    bug fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors; // <-- built-in CORS
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Enable CORS globally
-        $middleware->append(HandleCors::class);
+        // Prepend instead of append
+        $middleware->prepend(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 
+// Handle preflight OPTIONS request
+Route::options('/send-mail', function () {
+    return response()->noContent();
+});
+
+// Actual POST request
 Route::post('/send-mail', [MailController::class, 'sendMail']);
