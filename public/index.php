29ce6748d42579b0ec568e1b29e9e0fa7231632f
Esther Babaoye <estherbabaoye@gmail.com>
2025-08-15 02:23:34 +0100

    bug fix

--- public/index.php
+++ public/index.php
@@ -1,29 +1,11 @@
 <?php
-// --- Hard CORS guard (preflight + normal requests) ---
-$allowedOrigins = [
-    'https://esther-babaoye.netlify.app',
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-];
-
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins, true)) {
-    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-    header('Vary: Origin');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: *');
-    // header('Access-Control-Allow-Credentials: true'); // keep off unless using cookies
-}
-
-if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-// --- end hard CORS guard ---
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
+
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
